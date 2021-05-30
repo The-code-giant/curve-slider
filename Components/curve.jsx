28371796679be889848984b1a8e4a23ebc44ed43
mrhusseiny <mr.hosainy@gmail.com>
2021-05-30 14:06:08 +0430

    curve component initiated

---
+++ Components/curve.jsx
@@ -0,0 +1,5 @@
+import css from '../styles/curve.module.css'
+
+export default function Curve(props){
+    return (<canvas className={css.canvas} width="1200" height="600" id="curve_canvas_id"></canvas>)
+}
