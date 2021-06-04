4e686160b0df910d373d1b80b4402db001b98a3c
Ali  Alizada <akbar.alizadeh5@gmail.com>
2021-06-04 09:59:11 -0700

    update images and back button

--- Components/singleElement.js
+++ Components/singleElement.js
@@ -3,8 +3,8 @@ import styles from '../styles/SingleElement.module.css'
 export default function SingleElement(props) {
     const slideInfo = COMPONENT_DATA[props.selectedComponent]
     return (
-        <div id="abc" className={styles.wrapper}>
-            <a className={styles.back} onClick={props.onBack} href="#">Back to Maturity Curve</a>
+        <div id="abc" className={styles.wrapper} style={{ backgroundImage: `url(${slideInfo.imageUrl})` }}>
+            <a className={styles.back} onClick={props.onBack} href="#"><div/>Back to Maturity Curve</a>
             <div>
                 <div>
                     <h2>{slideInfo.title}</h2>
@@ -26,24 +26,28 @@ export default function SingleElement(props) {
 
 const COMPONENT_DATA = {
     Informational: {
+        imageUrl: '/slide/Informational.jpg',
         title: 'Informational',
         subTitle: 'level two',
         description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
         list: ['Data influenced decition culture', 'Data influenced decition culture', "Data influenced decition culture"]
     },
     Integrated: {
+        imageUrl: '/slide/Integrated.jpg',
         title: 'Integrated',
         subTitle: 'level two',
         description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
         list: ['Data influenced decition culture', 'Data influenced decition culture', "Data influenced decition culture"]
     },
     Digitalized: {
+        imageUrl: '/slide/Digitalized.jpg',
         title: 'Digitalized',
         subTitle: 'level two',
         description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
         list: ['Data influenced decition culture', 'Data influenced decition culture', "Data influenced decition culture"]
     },
     Automated: {
+        imageUrl: '/slide/Informational.jpg',
         title: 'Automated',
         subTitle: 'level two',
         description: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum",
